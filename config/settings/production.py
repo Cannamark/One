496caffb6f5e32b33875c20afe69dda8f26b3382
Rick Tesmond <tesmonrd@gmail.com>
2017-02-15 18:08:29 -0800

    added host

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,7 +65,8 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['cmrkinc.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['cmrkinc.com',
+                                                          'cannamarkinc.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
